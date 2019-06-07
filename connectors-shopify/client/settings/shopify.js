ae2269f8af65009f572ea4b733471891f3e5ac41
trojanh <rajan.tiwari@kiprosh.com>
2019-06-07 13:15:54 +0530

    Remove unwanted jquery checks

--- connectors-shopify/client/settings/shopify.js
+++ connectors-shopify/client/settings/shopify.js
@@ -24,26 +24,6 @@ Template.shopifyImport.events({
   "click [data-event-action=importDataFromShopify]"(event) {
     event.preventDefault();
 
-    if ($("#shopifyCheckboxCustomers").is(":checked") || $("#shopifyCheckboxProducts").is(":checked")) {
-      $(event.currentTarget).html(`<i class='fa fa-circle-o-notch fa-spin'></i> ${i18next.t("admin.shopifyConnectSettings.importing")}`);
-      event.currentTarget.disabled = true;
-    }
-
-    // If this is the primary shop, redirect to index
-    if (Reaction.getShopId() === Reaction.getPrimaryShopId()) {
-      Router.go("index");
-    } else {
-      const shopId = Reaction.getShopId();
-      const shop = Shops.findOne({ _id: shopId });
-
-      // Check to see if this shop has a slug, otherwise direct to shopId route
-      if (shop && shop.slug) {
-        Router.go(`/shop/${shop.slug}`);
-      } else {
-        Router.go(`/shop/${shopId}`);
-      }
-    }
-
     // If no option is selected, return error asking user to select type of import
     if (!$("#shopifyCheckboxCustomers").is(":checked") && !$("#shopifyCheckboxProducts").is(":checked")) {
       return Alerts.toast(i18next.t("admin.shopifyConnectSettings.chooseImportType"), "error");
