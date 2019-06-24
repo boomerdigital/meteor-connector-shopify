46910f3c0853df5ab1e9efb53a68592084cab55b
trojanh <rajan.tiwari@kiprosh.com>
2019-06-24 18:12:02 +0530

    Fetch product tags from Shopify

--- connectors-shopify/server/collections/shopifyTag.js
+++ connectors-shopify/server/collections/shopifyTag.js
@@ -4,6 +4,21 @@ import { Tracker } from "meteor/tracker";
 import { Tags } from "/lib/collections";
 import { registerSchema } from "@reactioncommerce/schemas";
 
+
+const RulesAttribute = new SimpleSchema({
+  column: {
+    type: String,
+    optional: true
+  },
+  relation: {
+    type: String,
+    optional: true
+  },
+  condition: {
+    type: String,
+    optional: true
+  }
+});
 /**
  * @file ShopifyTag
  *
@@ -18,20 +33,35 @@ import { registerSchema } from "@reactioncommerce/schemas";
  * @property {Number} shopifyId Shopify ID
  */
 export const ShopifyTag = new SimpleSchema({
+  // stores shopify collection sortOrder which can be used to control default sorting of products for this tag
   sortOrder: {
     type: String,
     optional: true
   },
+  // stores collection featured image from Shopify
   image: {
     type: String,
     optional: true
   },
+  // stores Shopify `body_html` property data
   description: {
     type: String,
     optional: true
-  }
+  },
+  // defines if a tag is visible to global or web or mobile scope
+  publishedScope: {
+    type: String,
+    optional: true
+  },
+  // stores shopify collection rules to handle tag searches
+  rules: {
+    type: Array,
+    optional: true
+  },
+
+  "rules.$": RulesAttribute
 }, { check, tracker: Tracker });
 
 registerSchema("ShopifyTag", ShopifyTag);
 
-Tags.attachSchema(ShopifyTag, { selector: { type: "simple" } });
+Tags.attachSchema(ShopifyTag);
